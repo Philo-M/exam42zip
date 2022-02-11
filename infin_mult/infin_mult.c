ad6da68cb65b5bf75a82d36625e4eed15299957d
IuriiMr <iurii.marushevskyi@gmail.com>
2022-02-11 05:58:29 +0100

    IM

--- infin_mult/infin_mult.c
+++ infin_mult/infin_mult.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: imarushe <imarushe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/04 21:50:10 by imarushe          #+#    #+#             */
-/*   Updated: 2022/02/06 11:25:24 by imarushe         ###   ########.fr       */
+/*   Created: 2022/02/10 22:08:59 by imarushe          #+#    #+#             */
+/*   Updated: 2022/02/11 05:52:09 by imarushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,95 +17,106 @@
 int	ft_strlen(char *str)
 {
 	int	i;
-	
+
 	i = 0;
 	while(str[i])
 		i++;
 	return (i);
 }
 
-int	main(int argc, char *argv[])
+int	ft_mult(char *s1, char *s2)
 {
-	(void)argc;
-
 	int		i;
+	int		j;
 	int		len1;
 	int		len2;
-	int		sum_len;
 	int		size;
 	int		minus1;
 	int		minus2;
 	char	*result;
 
-	if (argv[1][0] == '0' || argv[2][0] == '0')
-	{
-		write(1, "0\n", 2);
-		return (0);
-	}
-
-	len1 = ft_strlen(argv[1]);
-	len2 = ft_strlen(argv[2]);
-
 	minus1 = 0;
 	minus2 = 0;
-	if (argv[1][0] == '-')
-	{
+	if (s1[0] == '-')
 		minus1 = 1;
-		len1--;
-	}
-	if (argv[2][0] == '-')
-	{
+	if (s2[0] == '-')
 		minus2 = 1;
-		len2--;
-	}
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
 
-	sum_len = len1 + len2;
-	result = malloc(sizeof(char) * (sum_len + 1));
-	if (!result)
-		return (0);
-	i = 0;
-	while (i <= sum_len)
+	if (s1[0] != '0' && s2[0] != '0')
 	{
-		result[i] = 0;
-		i++;
-	}
-	result[sum_len] = '\0';
-
-
-	if (!minus2)
-		len2--;
-
-	i = 0;
-	while (len2 - i >= minus2)
-	{
-		size = sum_len - 1 - i;
-		len1 = ft_strlen(argv[1]) - 1;
-		while (len1 >= minus1)
+		size = len1 - minus1 + len2 - minus2;
+		result = malloc(sizeof(char) * (size + 1));
+		if (!result)
+			return (0);
+		result[size] = '\0';
+		i = 0;
+		while (i < size)
 		{
-			result[size] += (argv[1][len1] - 48) * (argv[2][len2 - i] - 48);
-			if (result[size] > 9)
-			{
-				result[size - 1] += result[size] / 10;
-				result[size] %= 10;
-			}
-			size--;
-			len1--;
+			result[i] = 0;
+			i++;
 		}
-		i++;
+		size--;
+		len1--;
+		len2--;
+
+		i = 0;
+		while (len2 - minus2 - i >= 0)
+		{
+			j = 0;
+			while (len1 - minus1 - j >= 0)
+			{
+				result[size - i - j] += (s2[len2 - i] - 48) * (s1[len1 - j] - 48);
+				if (result[size - i - j] > 9)
+				{
+					result[size - i - j - 1] += result[size - i - j] / 10;
+					result[size - i - j] %= 10;
+				}
+				j++;
+			}
+			i++;
+		}
+		if (minus1 - minus2)
+			write(1, "-", 1);
+		i = 0;
+		while (result[i] == 0)
+			i++;
+		while(i <= size)
+		{
+			result[i] += 48;
+			write(1, &result[i], 1);
+			i++;
+		}
+		free(result);
 	}
-	
-	i = 0;
-	if (minus1 + minus2  == 1)
-		write(1, "-", 1);
-	while (result[i] == 0)
-		i++;
-	while (i < sum_len)
-	{
-		result[i] += 48;
-		write(1, &result[i], 1);
-		i++;
-	}
+	else
+		write(1, "0", 1);
+	return (1);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 3)
+		ft_mult(argv[1], argv[2]);
 	write(1, "\n", 1);
-	free(result);
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
